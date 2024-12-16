36a80611058c7d7d92ba86ae958384cb292d001f
N1ckn1ght <thisefi@yandex.ru>
2024-12-16 17:06:26 +0800

    weights sync

--- src/engine/weights.rs
+++ src/engine/weights.rs
@@ -1,19 +1,36 @@
 pub struct Weights {
-    pub pre_mult_limit: i32,
+    pub possibility_limit: i32,
     pub local_threat_mult: i32,
     pub local_win_mult: i32,
-    pub core_board_value: i32,
-    pub global_threat_value: i32
+    pub global_emerging_threat_value: i32,
+    pub global_threat_value: i32,
+    pub soft_max: i32
 }
 
 impl Default for Weights {
     fn default() -> Self {
         Self {
-            pre_mult_limit: 4,
+            // local_possibility_add: 1,
+                // algorithm to count possibilities:
+                // 1. get every line from local board (total: 8)
+                // 2. if line contains current mark and is not blocked by other mark, add 1
+                // 3. save as max(count, possibility_limit)
+            possibility_limit: 4,
             local_threat_mult: 5,
+                // will overwrite possibilities, always counts as single threat
+            // global_possibility_add: 1,
+                // count global possibilities by the same algorithm
+                // note: no need to have a won local board; just need a possibility to win one
+                // multiply by it; e.g. if 0, then it's useless board after all
             local_win_mult: 9,
-            core_board_value: 9,
-            global_threat_value: 364
+                // will override anything on the board, of course
+            global_emerging_threat_mult: 2,
+                // this is a possibility with a local board that's already won
+                // if two boards are won in the row it'd be equal to two boards being won on two different lanes
+            global_threat_mult: 2,
+                // additional bonus for having a real threat, two boards in the row
+            soft_max: 8192,
+                // victory bonus
         }
     }
 }
