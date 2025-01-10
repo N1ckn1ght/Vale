81b5fc98df2c7ca4821e88917a50107b9384d60c
N1ckn1ght <thisefi@yandex.ru>
2025-01-10 14:13:44 +0800

    initial mapgen (working state)

--- src/engine/gen.rs
+++ src/engine/gen.rs
@@ -1,15 +1,14 @@
 use crate::frame::lookups::WIN_LOOKUP;
 
-pub const SHIFT_X_POS: u8 = 0;
-pub const SHIFT_X_UPO: u8 = 4;
-pub const SHIFT_X_THR: u8 = 8;
-pub const SHIFT_O_POS: u8 = 12;
-pub const SHIFT_O_UPO: u8 = 16;
-pub const SHIFT_O_THR: u8 = 20;
-pub const MASK_POS: u16 = 0b1111;
+pub const SHIFT_POS: u8 = 0;
+pub const SHIFT_UPO: u8 = 4;
+pub const SHIFT_THR: u8 = 8;
+pub const MASK_CNT: u16 = 0b1111;
 
-pub fn gen_maps() -> [u16; 0b1000000000000000000] {
-    let mut maps = [0; 0b1000000000000000000];
+// (x_maps, o_maps)
+pub fn gen_maps() -> (Vec<u16>, Vec<u16>) {
+    let mut x_maps = vec![0; 0b1000000000000000000];
+    let mut o_maps = vec![0; 0b1000000000000000000];
     
     for permut in 0..3_u32.pow(9) {
         let mut xbits: u16 = 0;
@@ -29,10 +28,10 @@ pub fn gen_maps() -> [u16; 0b1000000000000000000] {
         }
 
         let mut x_pos = 0;
-        // o_pos = 8 - x_upo
         let mut x_upo = 0;
-        // o_upo = 8 - x_pos
         let mut x_thr = 0;
+        let mut o_pos = 0;
+        let mut o_upo = 0;
         let mut o_thr = 0;
 
         let mut illegal = false;
@@ -56,12 +55,15 @@ pub fn gen_maps() -> [u16; 0b1000000000000000000] {
                 }
             }            
             if lookup & xbits == 0 {
+                o_pos += 1;
                 let os = (lookup & obits).count_ones();
-
                 match os {
                     0 => {},
-                    1 => {},
+                    1 => {
+                        o_upo += 1;
+                    },
                     2 => {
+                        o_upo += 1;
                         o_thr += 1;
                     },
                     _ => {
@@ -69,7 +71,7 @@ pub fn gen_maps() -> [u16; 0b1000000000000000000] {
                     }
                 }
             }
-            if illegal {
+            if illegal {    // aka if this board is already won by someone
                 break;
             }
         }
@@ -85,11 +87,13 @@ pub fn gen_maps() -> [u16; 0b1000000000000000000] {
 
         // bleh
         let key = ((obits as u32) << 9) | xbits as u32;
-        let value = (x_pos << SHIFT_X_POS) | (x_upo << SHIFT_X_UPO) | (x_thr << SHIFT_X_THR) | (o_pos << SHIFT_O_POS) | (o_upo << SHIFT_O_UPO) | (o_thr << SHIFT_O_THR);
-        maps[key as usize] = value;
+        let x_value = (x_pos << SHIFT_POS) | (x_upo << SHIFT_UPO) | (x_thr << SHIFT_THR);
+        let o_value = (o_pos << SHIFT_POS) | (o_upo << SHIFT_UPO) | (o_thr << SHIFT_THR);
+        x_maps[key as usize] = x_value;
+        o_maps[key as usize] = o_value;
     }
 
-    maps
+    (x_maps, o_maps)
 }
 
 
@@ -98,16 +102,25 @@ mod tests {
     use super::*;
 
     #[test]
-    fn gen_maps_manual_values_test() {
+    fn gen_maps_manual_sequential_test() {
         let maps = gen_maps();
 
-       assert_eq!(maps[0],  0);
-       assert_eq!(maps[0],  0);
-       assert_eq!(maps[0],  0);
-       assert_eq!(maps[0],  0);
-       assert_eq!(maps[0],  0);
-       assert_eq!(maps[0],  0);
-       assert_eq!(maps[0],  0);
-       assert_eq!(maps[0],  0);
+        let keys = [0b000000000000000000, 0b000000000000000001, 0b000000001000000000, 0b000000000000000010, 0b000000000000000011, 0b000000001000000010, 0b000000010000000000, 0b000000010000000001, 0b000000011000000000, 0b000000000000000100];
+        let values = [(8, 0, 0, 8, 0, 0), (8, 3, 0, 5, 0, 0), (5, 0, 0, 8, 3, 0), (8, 2, 0, 6, 0, 0), (8, 4, 1, 4, 0, 0), (5, 1, 0, 6, 2, 0), (6, 0, 0, 8, 2, 0), (6, 2, 0, 5, 1, 0), (4, 0, 0, 8, 4, 1), (8, 3, 0, 5, 0, 0)];
+        for (i, key) in keys.into_iter().enumerate() {
+            // println!("iter={}, key=[{:018b}]\nx=[{:09b}], o=[{:09b}]\n", i, key, maps.0[key], maps.1[key]);
+            let x_pos = (maps.0[key] >> SHIFT_POS) & MASK_CNT;
+            let x_upo = (maps.0[key] >> SHIFT_UPO) & MASK_CNT;
+            let x_thr = (maps.0[key] >> SHIFT_THR) & MASK_CNT;
+            let o_pos = (maps.1[key] >> SHIFT_POS) & MASK_CNT;
+            let o_upo = (maps.1[key] >> SHIFT_UPO) & MASK_CNT;
+            let o_thr = (maps.1[key] >> SHIFT_THR) & MASK_CNT;
+            assert_eq!(x_pos, values[i].0);
+            assert_eq!(x_upo, values[i].1);
+            assert_eq!(x_thr, values[i].2);
+            assert_eq!(o_pos, values[i].3);
+            assert_eq!(o_upo, values[i].4);
+            assert_eq!(o_thr, values[i].5);
+        }
     }
 }
